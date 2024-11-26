9a9f3d0e26cb1a4082985f95deea5695eea46461
N-Ghi <nagasaroghislaine3@gmail.com>
2024-11-26 14:31:51 +0200

    reset

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,9 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->singleton(GoogleService::class, function ($app) {
-            return new GoogleService();
-        });
+        //
     }
 
     /**
